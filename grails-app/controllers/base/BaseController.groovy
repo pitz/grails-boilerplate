dd6dd964887fefc5e99430f3655bc30071bff09d
Pitz <pitz.eduardo@gmail.com>
2020-02-21 13:08:19 -0300

    Criando métodos base para retornar response save: buildSaveResponse()

--- grails-app/controllers/base/BaseController.groovy
+++ grails-app/controllers/base/BaseController.groovy
@@ -14,21 +14,14 @@ class BaseController {
         return GsonBuilderUtils.buildClassFromJson(JsonOutput.toJson(requestJson), toDto)
     }
 
-
-    protected buildResponse(def entity, def responseDto) {
-        return responseBuilder(entity, responseDto)
-    }
-
-    private responseBuilder(def entity, def responseDto) {
-        responseDto.success = !Boolean.valueOf(entity.hasErrors())
-
-        if (responseDto.success) {
-            response.status = HttpStatus.OK.value()
-        } else {
+    protected Object buildSaveResponse(def entity, def responseDto) {
+        if (entity.hasErrors()) {
             response.status = HttpStatus.BAD_REQUEST.value()
             responseDto.errorMessage = entity.errors.allErrors[0].defaultMessage ?: message(code: entity.errors.allErrors[0].codes[0])
+        } else {
+            responseDto.id = entity?.publicId
+            response.status = HttpStatus.OK.value()
         }
-
         return responseDto
     }
 }
