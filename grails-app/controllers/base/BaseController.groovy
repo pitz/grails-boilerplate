2a664eabd95bb9260c5a99ccc6a027635ac6c193
Pitz <pitz.eduardo@gmail.com>
2020-02-21 13:26:55 -0300

    Alterando método para buildResponse()

--- grails-app/controllers/base/BaseController.groovy
+++ grails-app/controllers/base/BaseController.groovy
@@ -1,10 +1,7 @@
 package boilerplate.controller.base
 
+import boilerplate.dto.base.BaseResponseDTO
 import boilerplate.utils.GsonBuilderUtils
-import com.google.gson.GsonBuilder
-
-import grails.converters.JSON
-import grails.validation.ValidationException
 import groovy.json.JsonOutput
 import org.springframework.http.HttpStatus
 
@@ -14,7 +11,11 @@ class BaseController {
         return GsonBuilderUtils.buildClassFromJson(JsonOutput.toJson(requestJson), toDto)
     }
 
-    protected Object buildSaveResponse(def entity, def responseDto) {
+    protected Object buildResponse(def entity) {
+        return buildResponse(entity, new BaseResponseDTO())
+    }
+
+    protected Object buildResponse(def entity, def responseDto) {
         if (entity.hasErrors()) {
             response.status = HttpStatus.BAD_REQUEST.value()
             responseDto.errorMessage = entity.errors.allErrors[0].defaultMessage ?: message(code: entity.errors.allErrors[0].codes[0])
